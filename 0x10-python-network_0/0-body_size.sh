81e60bd16afef1b7bc3f48d96dacd26ce9e05ec8
Nobleman20 <emmanueloluyege@gmail.com>
2023-04-27 15:21:14 -0700

    Task0 fix1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,6 @@
 
 # By Emmanuel Oluyege
 
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+# Get the byte size of the HTTP response header for a given URL
+
+echo "$(curl -s -w '%{size_download}' -o /dev/null $1)".
